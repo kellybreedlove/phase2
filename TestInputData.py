3ad73055425bd0c9af51c0cea39ac442bc3be545
Kelly Breedlove <kelly.breedlove@my.wheaton.edu>
2015-03-27 10:48:58 -0500

    added test for input data

---
+++ TestInputData.py
@@ -0,0 +1,20 @@
+from PyCamellia import *
+from InputData import *
+import unittest
+
+
+class TestInputData(unittest.TestCase):
+
+    """Test Some Stuff"""
+    def test_Stuff(self):
+        pass
+
+
+
+
+    if __name__ == '__main__':
+        unittest.main()
+
+
+
+
