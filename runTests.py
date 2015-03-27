110664aee4b9544c27bb664f80214f540de00ee2
Kelly Breedlove <kelly.breedlove@my.wheaton.edu>
2015-03-27 16:17:22 -0500

    added tests for plotter

--- runTests.py
+++ runTests.py
@@ -1,10 +1,12 @@
 from TestFunctionParser import *
 from TestConditionParser import * 
-from TestInputData import *
+#from TestInputData import *
+from TestPlotter import *
 import unittest
 
 testSuite = unittest.makeSuite(TestFunctionParser)
-testSuite.addTest(unittest.makeSuite(TestInputData))
+#testSuite.addTest(unittest.makeSuite(TestInputData))
+testSuite.addTest(unittest.makeSuite(TestPlotter))
 #testSuite.addTest(unittest.makeSuite(TestConditionParser))
 
 testRunner = unittest.TextTestRunner()
