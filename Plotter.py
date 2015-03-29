52148efdf60dae67d60700821e038ed4cae9b951
phaefliger <paul.haefliger@my.wheaton.edu>
2015-03-28 22:42:11 -0500

    Added plotError

--- Plotter.py
+++ Plotter.py
@@ -6,8 +6,34 @@ import itertools
 
 debug = False
 
-def plotError(perCellError, mesh,title=""):
-    pass
+def plotError(perCellError, mesh, title=""):
+    meshX = []
+    meshY = []
+    tempCell = []
+    #for each active cell
+    for cellID in cellIds:
+        tempCell = mesh.verticesForCell(cellID)
+        #for each separate vertex of the cell
+        for vert in tempCell:
+           meshX.append(vert[0]) #get the x value for the vertex
+           meshY.append(vert[1])
+           
+    #dummy color values for the plot as to be 0
+    meshX = sorted(list(set(meshX))) #sort and remove duplicates 
+    meshY = sorted(list(set(meshY))) #sort and remove duplicates
+    meshX = around(meshX, decimals = 3) #round all x values to 3 decimal places
+    meshY = around(meshY, decimals = 3) #round all y values to 3 decimal places
+    #make the actual mesh plot
+    plt.pcolormesh(array(meshX), array(meshY), perCellError, edgecolors='k', linewidths=2, 
+                       cmap='bwr', vmin='-100', vmax='100') 
+
+    plt.xticks(meshX) #plot the ticks on the x axis with all x points
+    plt.yticks(meshY) #plot the ticks on the y axis with all y points
+    plt.xlim(0, meshX[len(meshX)-1]) #limit the x axis to the maximum mesh dimension
+    plt.ylim(0, meshY[len(meshY)-1]) #limit the y axis to the minimum mesh dimension
+    plt.title(title)
+    plt.show() #show the plot
+    
 
 def plotMesh(cellIds, mesh,title=""): 
     meshX = []
@@ -31,17 +57,17 @@ def plotMesh(cellIds, mesh,title=""):
     plt.pcolormesh(array(meshX), array(meshY), colA, edgecolors='k', linewidths=2, 
                        cmap='bwr', vmin='-100', vmax='100') 
 
-    plt.title(title)
     plt.xticks(meshX) #plot the ticks on the x axis with all x points
     plt.yticks(meshY) #plot the ticks on the y axis with all y points
     plt.xlim(0, meshX[len(meshX)-1]) #limit the x axis to the maximum mesh dimension
     plt.ylim(0, meshY[len(meshY)-1]) #limit the y axis to the minimum mesh dimension
+    plt.title(title)
     plt.show() #show the plot
     
     
     
     
-def plot(values,pointsArray,title=""):
+def plot(values,pointsArray, title=""):
     xCoor = []
     yCoor = []
     mergedVals = list(itertools.chain.from_iterable(values))
@@ -59,6 +85,7 @@ def plot(values,pointsArray,title=""):
         print len(xCoor)
         print len(yCoor)
 
-    plt.title(title)
+
     plt.hexbin(xCoor,yCoor,mergedVals,cmap='bwr', vmin=min(mergedVals),vmax=max(mergedVals),mincnt=0)
+    plt.title(title)
     plt.show()
