e9dca78150bb9882220ff654d79064d41644c29e
Jeffrey D. Burge <jeffrey.burge@cslab14.wheaton.edu>
2015-03-28 21:04:56 -0500

    updated runPlotTests.py

--- runPlotTests.py
+++ runPlotTests.py
@@ -1,7 +1,12 @@
 from TestPlotter import *
+from TestPlotterError import *
+from TestPlotterP import *
 import unittest
 
 testSuite = unittest.makeSuite(TestPlotter)
+testSuite.addTest(unittest.makeSuite(TestPlotterError)
+testSuite.addTest(unittest.makeSuite(TestPlotterP)
+
 
 testRunner = unittest.TextTestRunner()
 testRunner.run(testSuite)
