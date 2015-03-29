401cf1b70aee09dd90ea72b40ad54086ceeee348
Jeffrey D. Burge <jeffrey.burge@cslab10.wheaton.edu>
2015-03-29 12:13:37 -0500

    fixed a few typos

--- TestRefine.py
+++ TestRefine.py
@@ -2,11 +2,14 @@ from Refine import *
 from PyCamellia import *
 import unittest
 
+#########################  For testing States for Refine input
+#########################  Note that refine methods already tested
+#########################  in TestSolutionFns
 class TestRefine(unittest.TestCase):
     
     """Test Some Stuff"""
     def test_stuff(self):
-        self.assertTrue(False) # need to write tests
+        pass
 
     if __name__ == '__main__':
         unittest.main()
