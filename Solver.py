3e7a8220a2d7297a6488cfa0bdf826cb64ddfef6
Kelly Breedlove <kelly.breedlove@my.wheaton.edu>
2015-03-29 15:02:11 -0500

    stupid capitalization problem

--- Solver.py
+++ Solver.py
@@ -286,7 +286,7 @@ class SaveState:
 		    saveFile.close()
 		
 		    print("saved.")
-		    return PostsolveState.Instance()
+		    return PostSolveState.Instance()
 
 #run solver
 if __name__ == '__main__':
